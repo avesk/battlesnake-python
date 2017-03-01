fcee07f08036d21a28c72e12df045359d45d971d
avesk <averykushner@gmail.com>
2017-02-28 20:12:05 -0800

    test

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ def start():
 def move():
     data = bottle.request.json
 
-    dir = check_collisions(data['coords'])
+    dir = check_collisions(data["coords"])
 
     # TODO: Do things with data
     directions = ['up', 'down', 'left', 'right']
