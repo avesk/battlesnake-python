a7d020686f29438efdf0d3ef51d056055b60d693
avesk <averykushner@gmail.com>
2017-02-28 19:54:11 -0800

    test

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#FFFFFF',
+        'color': '#00FF00',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
         'name': 'battlesnake-python'
@@ -33,36 +33,14 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-    coords = data['coords']
 
-    # dir = check_collisions(coords)
+    # TODO: Do things with data
     directions = ['up', 'down', 'left', 'right']
-    dir = directions[2]
-    
+
     return {
         'move': random.choice(directions),
         'taunt': 'battlesnake-python!'
     }
-
-# def check_collisions(coords):
-#     head = coords[0]
-#     x = head[0]
-#     y = head[1]
-#     horiz = {'left' : x-1, 'right' : x+1}
-#     vert = {'up' : y+1, 'down' : y-1}
-
-#     for coord in coords:
-#         if coord != [ horiz['left'], y ]
-#             return 'left'
-#         if coord != [ horiz['right'], y ]
-#             return 'right'
-#         if coord != [ x, vert['up'] ]
-#             return 'up'
-#         if coord != [ x, vert['down'] ]
-#             return 'down'
-
-#         else return 'down'
-
 
 
 # Expose WSGI app (so gunicorn can find it)
