84577b296157549f9b13ab829904928b1ec35c9f
avesk <averykushner@gmail.com>
2017-02-28 19:15:19 -0800

    first algorithm attmempt

--- app/main.py
+++ app/main.py
@@ -33,14 +33,35 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
+    coords = data['coords']
 
-    # TODO: Do things with data
+    dir = check_collisions(coords)
     directions = ['up', 'down', 'left', 'right']
-
+    
     return {
-        'move': random.choice(directions),
+        'move': dir,
         'taunt': 'battlesnake-python!'
     }
+
+def check_collisions(coords):
+    head = coords[0]
+    x = head[0]
+    y = head[1]
+    horiz = {'left' : x-1, 'right' : x+1}
+    vert = {'up' : y+1, 'down' : y-1}
+
+    for coord in coords:
+        if coord != [ horiz['left'], y ]
+            return 'left'
+        if coord != [ horiz['right'], y ]
+            return 'right'
+        if coord != [ x, vert['up'] ]
+            return 'up'
+        if coord != [ x, vert['down'] ]
+            return 'down'
+
+        else return 'down'
+
 
 
 # Expose WSGI app (so gunicorn can find it)
