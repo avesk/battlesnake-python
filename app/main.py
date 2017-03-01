83dbe1cb19f35a3b45586217f6ade5dc148e9614
avesk <averykushner@gmail.com>
2017-02-28 19:36:45 -0800

    change color

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#00FF00',
+        'color': '#FFFFFF',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
         'name': 'battlesnake-python'
